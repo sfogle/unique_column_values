c9c8ca880e542f8440eba8af7829072c82b4ae26
Sean Fogle <sean.fogle@gmail.com>
2017-12-01 20:43:35 -0600

    refactor: extract method

--- unique_column_values.py
+++ unique_column_values.py
@@ -2,7 +2,7 @@ import csv
 import argparse
 
 
-def main(csv_file_name, column_number):
+def print_unique_column_values(csv_file_name, column_number):
     values = []
     with open(csv_file_name, 'r') as f:
         reader = csv.reader(f)
@@ -12,11 +12,15 @@ def main(csv_file_name, column_number):
         print(value)
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(
         description=('Get the unique values '
                      'of a particular column from a csv file.'))
     parser.add_argument('csv_file_name')
     parser.add_argument('column_number', type=int)
     args = parser.parse_args()
-    main(args.csv_file_name, args.column_number)
+    print_unique_column_values(args.csv_file_name, args.column_number)
+
+
+if __name__ == '__main__':
+    main()
